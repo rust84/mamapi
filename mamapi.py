36a840103d37b5379e2db9c5ee2c1c8b6bbdd36d
elforkhead <elforkhead@proton.me>
2025-05-07 22:31:27 -0400

    small text change

--- mamapi.py
+++ mamapi.py
@@ -170,7 +170,7 @@ def processResponse(jsonResponse):
         logger.critical("The script will now exit")
         sys.exit(1)
     elif json_response_msg == "Invalid session".casefold():
-        logger.critical("Per MAM: 'The system deemed the session invalid (bad mam_id value, or you've moved off the locked IP/ASN.)'")
+        logger.critical("Per MAM: 'The system deemed the session invalid (bad mam_id value, or you've moved off the locked IP/ASN)'")
         logger.critical("ASN locked sessions sometimes produce this error")
         logger.critical("Sometimes sessions are randomly invalidated")
         logger.critical("The script will now exit")
