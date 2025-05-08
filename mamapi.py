93b64e06d54fe7068b87d4942a9c33ef1d884e33
elforkhead <elforkhead@proton.me>
2025-05-07 22:47:03 -0400

    more bet hedging

--- mamapi.py
+++ mamapi.py
@@ -172,9 +172,7 @@ def processResponse(jsonResponse):
     elif json_response_msg == "Invalid session".casefold():
         logger.critical("Per MAM: 'The system deemed the session invalid (bad mam_id value, or you've moved off the locked IP/ASN)'")
         logger.critical("This can be a complicated issue to solve")
-        logger.critical("ASN locked sessions sometimes produce this error")
-        logger.critical("Sometimes enabling ASN locking helps")
-        logger.critical("Sometimes sessions are randomly invalidated")
+        logger.critical("Toggling ASN locking on or off sometimes helps")
         logger.critical("The script will now exit")
         sys.exit(1)
     elif json_response_msg == "No Session Cookie".casefold():
